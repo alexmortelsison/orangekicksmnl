01d6cc9b85a80dc471c7e84e7b15defdb711e0c1
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-09-13 10:25:17 +0900

    updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { ReactComponent as NikeLogo } from "../assets/nike-logo.svg";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 const ROUTES = ["Home", "About", "Service", "Pricing", "Contact"];
+
 const Nav = () => {
   return (
     <nav className="flex flex-wrap justify-between">
-      <a href="">
-        <NikeLogo className="h-20 w-20"></NikeLogo>
+      <a>
+        <NikeLogo className="h-20 w-20" />
       </a>
-      <button className="focus:ring-2 focus:ring-gray-200 p-2 rounded-lg hover:bg-gray-100">
-        <RxHamburgerMenu size={25}></RxHamburgerMenu>
+      <button className="focus:ring-2 focus:ring-gray-100 rounded-lg p-2 hover:bg-gray-100">
+        <RxHamburgerMenu size={25} />
       </button>
-
       <div className="w-full">
-        <ul className="bg-gray-50 text-lg border border-gray-100 rounded-lg p-4">
+        <ul>
           {ROUTES.map((route, i) => {
             return (
               <li
-                className={`rounded cursor-pointer py-2 px-3 ${i === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                className={` rounded cursor-pointer px-3 py-2 ${
+                  i === 0 ? "bg-blue-500" : "hover:bg-gray-100"
+                }`}
                 key={route}
               >
                 {route}
